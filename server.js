af2e67cc4e39c5fcae23eeda0d885158edaae402
RyosukeSomeya <ryosuke.someya.0618.dev@gmail.com>
2023-10-24 00:58:49 +0900

    xss、cspについて

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
+const crypto = require('crypto');
 const express = require('express');
 const api = require('./routes/api');
 
 const app = express();
 const port = 3000;
+
+app.set('view engine', 'ejs');
 
 app.use(express.static('public'));
 
@@ -10,6 +13,19 @@ app.use('/api', api);
 
 app.get('/', (req, res, next) => {
   res.end('Top Page');
+});
+
+app.get('/csp', (req, res, next) => {
+  const nonceValue = crypto.randomBytes(16).toString('base64');
+
+  res.header(
+    'Content-Security-Policy',
+    `script-src 'nonce-${nonceValue}' 'strict-dynamic';` +
+      "object-src 'none';" +
+      "base-uri 'none';" +
+      "require-trusted-types-for 'script'"
+  );
+  res.render('csp', { nonce: nonceValue });
 });
 
 // サーバー起動
